b7fdf05bc1d2e30a8439b04b6ff48b5675a8a297
vladigor <vladigor@gmail.com>
2026-06-19 07:20:17 +0100

    Visual stats

--- eslint.config.js
+++ eslint.config.js
@@ -21,7 +21,7 @@ export default [
     rules: {
       'linebreak-style': 'off',  // Handled by pre-commit's end-of-file-fixer
       'semi': ['error', 'always'],
-      'no-unused-vars': ['warn', {
+      'no-unused-vars': ['error', {
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_',
         caughtErrorsIgnorePattern: '^_|err'
